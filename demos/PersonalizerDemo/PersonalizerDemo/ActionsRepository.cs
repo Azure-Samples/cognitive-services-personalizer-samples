d14680792da0933ef17f76db6ef2ffb1746fff26
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-05-03 20:37:54 -0300

    Add Personalizer Demo

---
+++ demos/PersonalizerDemo/PersonalizerDemo/ActionsRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Personalization;
+using Microsoft.Azure.CognitiveServices.Personalization.Models;
+using Newtonsoft.Json;
+using PersonalizerDemo.ActionFeaturizer;
+using PersonalizerDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalizerDemo
+{
+    public class ActionsRepository : IActionsRepository
+    {
+        private IList<RankableActionWithMetadata> _actions = new List<RankableActionWithMetadata>();
+        private IList<RankableActionWithMetadata> _actionsWithTextAnalytics = new List<RankableActionWithMetadata>();
+
+        public ActionsRepository(IHostingEnvironment hostingEnvironment, IActionFeaturizer actionFeaturizer)
+        {
+            var fileProvider = hostingEnvironment.ContentRootFileProvider;
+            var contents = fileProvider.GetDirectoryContents("articles");
+            var articles = contents
+                .Select(file => System.IO.File.ReadAllText(file.PhysicalPath))
+                .Select(fileContent => JsonConvert.DeserializeObject<Models.Article>(fileContent));
+
+            CreateRankableActions(articles, actionFeaturizer);
+        }
+
+        public IList<RankableAction> GetActions(bool useTextAnalytics)
+        {
+            return useTextAnalytics ? _actionsWithTextAnalytics.Cast<RankableAction>().ToList() : _actions.Cast<RankableAction>().ToList();
+        }
+
+        public IList<RankableActionWithMetadata> GetActionsWithMetadata(bool useTextAnalytics)
+        {
+            return useTextAnalytics ? _actionsWithTextAnalytics : _actions;
+        }
+
+        private void CreateRankableActions(IEnumerable<Article> articles, IActionFeaturizer actionFeaturizer)
+        {
+            foreach (var article in articles)
+            {
+                CreateRankableAction(article, actionFeaturizer).Wait();
+            }
+
+            _actions = _actions.OrderBy(a => a.Id).ToList();
+            _actionsWithTextAnalytics = _actionsWithTextAnalytics.OrderBy(a => a.Id).ToList();
+        }
+
+        private async Task CreateRankableAction(Article article, IActionFeaturizer actionFeaturizer)
+        {
+            this._actions.Add(new RankableActionWithMetadata(article));
+
+            var rankableAction = new RankableActionWithMetadata(article);
+            var features = await actionFeaturizer.FeaturizeActionsAsync(article);
+            foreach (var feature in features)
+            {
+                rankableAction.Features.Add(feature);
+            }
+
+            this._actionsWithTextAnalytics.Add(rankableAction);
+        }
+    }
+}
---
+++ demos/PersonalizerDemo/PersonalizerDemo/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Personalization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using PersonalizerDemo.ActionFeaturizer;
+
+namespace PersonalizerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var personalizationEndPoint = Configuration["PersonalizationEndpoint"];
+            var personalizationApiKey = Configuration["PersonalizationApiKey"];
+            var cognitiveTextAnalyticsSubscriptionKey = Configuration["TextAnalyticsKey"];
+            var cognitiveTextAnalyticsEndpoint = Configuration["TextAnalyticsEndpoint"];
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSingleton<ITextAnalyticsClient>(s => new TextAnalyticsClient(new ApiKeyServiceClientCredentials(cognitiveTextAnalyticsSubscriptionKey))
+            {
+                // Cognitive Service Endpoint
+                Endpoint = cognitiveTextAnalyticsEndpoint.Split("/text/analytics")[0]
+            });
+            services.AddSingleton<CognitiveTextAnalyzer, CognitiveTextAnalyzer>();
+            services.AddSingleton<IActionFeaturizer, CognitiveTextAnalyticsFeaturizer>();
+            services.AddSingleton<IPersonalizationClient>(s => CreateClient(personalizationEndPoint, personalizationApiKey));
+            services.AddSingleton<IActionsRepository, ActionsRepository>();
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private IPersonalizationClient CreateClient(string uri, string ApiKey)
+        {
+            return new PersonalizationClient(
+                new Uri(uri),
+                new ApiKeyServiceClientCredentials(ApiKey),
+                new DelegatingHandler[] { }
+            );
+        }
+    }
+}
