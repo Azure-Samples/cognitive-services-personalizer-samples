edbb1ad31b608025572719fbec146a46e20ff6b4
Lucas Rojas <lucas.rojas@southworks.com>
2019-07-05 13:13:03 -0300

    Add navbar color config to json

--- demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/Models/NavBarOptionsModel.cs
+++ demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/Models/NavBarOptionsModel.cs
@@ -8,6 +8,7 @@ namespace PersonalizerBusinessDemo.Models
     public class NavBarOptionsModel
     {
         public string LogoImageURL { get; set; }
+        public string BackgroundColor { get; set; }
         public List<NavBarItemModel> OptionsList { get; set; }
     }
 }
