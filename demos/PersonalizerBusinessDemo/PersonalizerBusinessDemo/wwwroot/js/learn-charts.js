80d2ed5b2711af60189d634dc26f221e553f24b6
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-07-10 23:46:40 -0300

    Add other charts and play button

--- demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/wwwroot/js/learn-charts.js
+++ demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/wwwroot/js/learn-charts.js
@@ -10,20 +10,30 @@
     let data = [];
     let currentValue = 0;
     const maxValue = 0.8;
-    for (i = 1; i <= maxLoop / hoops; i++) {
-        let max = currentValue + 0.03;
-        let min = currentValue - 0.01;
-        
-        currentValue = Math.random() * (max - min) + min;
-        if (currentValue > maxValue) {
-            currentValue = maxValue;
+
+    function getRandomValue(currentValue, maxDelta, minDelta) {
+        let max = currentValue + maxDelta;
+        let min = currentValue - minDelta;
+
+        let newValue = Math.random() * (max - min) + min;
+        if (newValue > maxValue) {
+            return maxValue;
         }
+
+        return newValue;
+    }
+
+    for (i = 1; i <= maxLoop / hoops; i++) {
+        currentValue = getRandomValue(currentValue, 0.03, 0.01);
 
         data.push(currentValue);
     }
 
-    const ctx = document.getElementById('avg-learn-chart');
-    const avgLearnChart = new Chart(ctx, {
+    const startLearnBtnEle = document.getElementById("start-learn-btn");
+    const currentAvgNumberEle = document.getElementById('current-avg-number');
+
+    const avgCtx = document.getElementById('avg-learn-chart');
+    const avgLearnChart = new Chart(avgCtx, {
         type: 'line',
         data: {
             labels: labels,
@@ -55,23 +65,91 @@
         }
     });
 
-    function addData(chart, data) {
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-        });
-        chart.update();
+
+    const peopleCtx = document.getElementById('people-learn-chart');
+    const peopleChart = new Chart(peopleCtx, {
+        type: 'bar',
+        data: {
+            labels: ["P1", "P2", "P3", "P4"],
+            datasets: [{
+                data: [0, 0, 0, 0],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        maxRotation: 90,
+                        minRotation: 80
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0,
+                        max: 1,
+                        stepSize: 0.1,
+                        suggestedMin: 0,
+                        suggestedMax: 1
+                    }
+                }]
+            }
+        }
+    });
+
+    function updateData(avgLearnChart, peopleChart,  data, currentTick) {
+        avgLearnChart.data.datasets[0].data.push(data);
+        avgLearnChart.update();
+               
+        peopleChart.data.datasets[0].data = [
+            getRandomValue(data, 0.05, 0.05),
+            getRandomValue(data, 0.05, 0.05),
+            getRandomValue(data, 0.05, 0.05),
+            getRandomValue(data, 0.05, 0.05)
+        ];
+        peopleChart.update();
+
+        currentAvgNumberEle.innerHTML = parseFloat(Math.round(data * 100) / 100).toFixed(2);
     }
 
     const maxTick = maxLoop / hoops;
+    let intervalId = -1;
 
-    let currentTick = 0;
-    let intervalId = setInterval(function () {
-        if (currentTick >= maxTick) {
+    startLearnBtnEle.addEventListener("click", function () {
+        if (intervalId >= 0) {
             clearInterval(intervalId);
         }
 
-        addData(avgLearnChart, data[currentTick]);
-        currentTick++;
+        avgLearnChart.data.datasets[0].data = [];
+        avgLearnChart.update();
 
-    }, 70);
+        let currentTick = 0;
+        intervalId = setInterval(function () {
+            if (currentTick >= maxTick) {
+                clearInterval(intervalId);
+                return;
+            }
+
+            updateData(avgLearnChart, peopleChart, data[currentTick], currentTick);
+            currentTick++;
+
+        }, 70);
+    });
 });
