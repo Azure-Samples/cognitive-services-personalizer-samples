e68e3c94062fe206a76fef30251031ff5673075c
Ezequiel Jadib <ezequiel.jadib@southworks.com>
2019-07-11 09:27:28 -0300

    Update links at the bottom and explore/exploits tooltips

--- demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/Models/PageConfigModel.cs
+++ demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/Models/PageConfigModel.cs
@@ -1,17 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace PersonalizerBusinessDemo.Models
 {
     public class PageConfigModel
     {
         public string RefreshButtonText { get; set; }
 
+        public string PersonalizerSignUpURL { get; set; }
+
+        public string PersonalizerSignUpLabel { get; set; }
+
         public string PersonalizerDocumentationURL { get; set; }
 
         public string PersonalizerDocumentationLabel { get; set; }
+
+        public string PersonalizerDeveloperDocsURL { get; set; }
+
+        public string PersonalizerDeveloperDocsLabel { get; set; }
+
+        public string PersonalizerCodeSamplesURL { get; set; }
+
+        public string PersonalizerCodeSampleLabel { get; set; }
 
         public NavBarOptionsModel NavigationBar { get; set; }
 
