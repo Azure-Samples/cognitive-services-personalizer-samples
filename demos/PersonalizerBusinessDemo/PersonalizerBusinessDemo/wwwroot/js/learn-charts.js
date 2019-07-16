f48bd2b112ba3046d6801f9a1467ed04adc31349
Lucas Rojas <lucas.rojas@southworks.com>
2019-07-16 13:39:16 -0300

    Add labels to the bar chart

--- demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/wwwroot/js/learn-charts.js
+++ demos/PersonalizerBusinessDemo/PersonalizerBusinessDemo/wwwroot/js/learn-charts.js
@@ -98,20 +98,18 @@
     const peopleChart = new Chart(peopleCtx, {
         type: 'bar',
         data: {
-            labels: ["User 1", "user 2", "User 3", "User 4"],
+            labels: ["Mobile", "Social Media", "Anonymous Users"],
             datasets: [{
-                data: [0, 0, 0, 0],
+                data: [0, 0, 0],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)'
+                    'rgba(255, 206, 86, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255,99,132,1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
+                    'rgba(255, 206, 86, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -128,8 +126,9 @@
             scales: {
                 xAxes: [{
                     ticks: {
-                        maxRotation: 90,
-                        minRotation: 80
+                        autoSkip: false,
+                        maxRotation: 0,
+                        minRotation: 0
                     }
                 }],
                 yAxes: [{
