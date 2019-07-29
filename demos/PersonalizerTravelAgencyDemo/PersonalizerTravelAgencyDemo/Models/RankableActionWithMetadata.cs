9c20af737806d200e910da7bd322b6d33c8c95ad
Lucas Rojas <lucas.rojas@southworks.com>
2019-07-29 12:08:15 -0300

    Refactor RankableActionWithMetadata

--- demos/PersonalizerTravelAgencyDemo/PersonalizerTravelAgencyDemo/Models/RankableActionWithMetadata.cs
+++ demos/PersonalizerTravelAgencyDemo/PersonalizerTravelAgencyDemo/Models/RankableActionWithMetadata.cs
@@ -7,43 +7,15 @@ namespace PersonalizerTravelAgencyDemo.Models
 {
     public class RankableActionWithMetadata : RankableAction
     {
-        public RankableActionWithMetadata(Article article)
+        public RankableActionWithMetadata(Action action)
         {
-            Id = article.Id;
-            ImageName = article.ImageName;
-            Title = article.Title;
-            Features = new List<object>()
-            {
-                    new {article.Source},
-                    new {article.HasVideo},
-                    new {article.PublishedAgo}
-            };
-
-            if (article.FeaturedByEditorial.HasValue)
-            {
-                Features.Add(new { article.FeaturedByEditorial });
-            }
-
-            if (article.Entities != null && article.Entities.Any())
-            {
-                Features.Add(new { article.Entities });
-            }
-
-            if (article.Tags != null && article.Tags.Any())
-            {
-                ExpandoObject tags = new ExpandoObject();
-
-                foreach (string tag in article.Tags)
-                {
-                    tags.TryAdd(tag, "Yes");
-                }
-
-                Features.Add(new { tags });
-            }
+            Id = action.Id;
+            Image = action.Image;
+            
         }
 
         public string Title { get; set; }
 
-        public string ImageName { get; set; }
+        public string Image { get; set; }
     }
 }
