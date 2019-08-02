192ac1ca5ee2ccd32dfed8fc46b59710026e1d2f
Kevin Bronstein <kevin.bronstein@southworks.com>
2019-08-02 17:59:42 -0300

    Add configuration files for dynamic text in checkout page

---
+++ demos/PersonalizerTravelAgencyDemo/PersonalizerTravelAgencyDemo/Models/CheckoutPageTexts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalizerTravelAgencyDemo.Models
+{
+    public class CheckoutPageText
+    {
+        public string Title { get; set; }
+        public string PackageTitle { get; set; }
+        public List<string> PackageItems { get; set; }
+        public string CostsTitle { get; set; }
+        public List<string> CostsItems { get; set; }
+        public string DiscoverTitle { get; set; }
+        public List<string> DiscoverItems { get; set; }
+        public string PaymentTitle { get; set; }
+        public string PaymentText { get; set; }
+        public string ConfirmButtonLabel { get; set; }
+        public string SaveForLaterLinkLabel { get; set; }
+    }
+}
