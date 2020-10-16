c6b7f1a0d96fc60d7b67bf0e0a7676f4b39fb2ed
Lily Motley <limotley@microsoft.com>
2020-10-16 15:14:10 -0700

    Upgraded Text Analytics nuget package

--- demos/PersonalizerDemo/PersonalizerDemo/Startup.cs
+++ demos/PersonalizerDemo/PersonalizerDemo/Startup.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
